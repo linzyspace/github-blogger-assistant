4b33d4e413a90927ca168260735f49784f73ec9a
linzyspace <linzyspace@gmail.com>
2025-12-02 08:07:57 -0800

    Update assistant.py

--- app/assistant.py
+++ app/assistant.py
@@ -1,1 +1,14 @@
+from responses import get_predefined_response
+from blog_lookup import search_blog_posts
+
+def get_predefined_or_blog_response(text: str, lang: str = "en"):
+    res = get_predefined_response(text, lang)
+    if res:
+        return {"type": "predefined", "response": res}
+
+    posts = search_blog_posts(text)
+    if posts:
+        return {"type": "blog_match", "posts": posts}
+
+    return None
 
