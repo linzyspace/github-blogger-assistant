b67f641d1181b11276112cb5bb290b953d9ec52d
linzyspace <linzyspace@gmail.com>
2025-12-04 06:46:25 -0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+
+class Config:
+    PORT = int(os.getenv("PORT", 5000))
+    BLOG_ID = os.getenv("BLOG_ID")
+    API_KEY = os.getenv("API_KEY")
+
+config = Config()
 
