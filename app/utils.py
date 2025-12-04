81eb875b9dc717f496d61ef0e10fb0a4263e1440
linzyspace <linzyspace@gmail.com>
2025-12-03 17:27:11 -0800

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -5,10 +5,6 @@ from typing import Any
 BASE_DIR = pathlib.Path(__file__).parent
 
 def load_yaml(filename: str) -> Any:
-    """
-    Load a YAML file from app/data safely.
-    """
     path = BASE_DIR / "data" / filename
     with path.open("r", encoding="utf-8") as f:
         return yaml.safe_load(f)
-
