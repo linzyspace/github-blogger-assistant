40b2dae138edd5a7cb6f54a33606cd23b3abe66e
linzyspace <linzyspace@gmail.com>
2025-12-07 22:42:55 -0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 8080,
   BLOG_ID: process.env.BLOG_ID,
   API_KEY: process.env.API_KEY
 };
